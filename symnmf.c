c03c60cc9f9b21d649607270a451f785dbaab904
LihieNiv <lihieniv@mail.tau.ac.il>
2024-11-11 19:26:47 +0200

    added mult_mat and changed data to cord/vector structure

--- symnmf.c
+++ symnmf.c
@@ -4,31 +4,61 @@
 
 // Make sure all numbers use double
 
-double **ddg(double **data, int n)
+typedef struct cord
 {
-    // Computes diagonal degree matrix
+    double value;
+    cord *next;
+} cord;
+
+typedef struct vector
+{
+    vector *next;
+    cord *cords;
+} vector;
+
+// Computes diagonal degree matrix
+double **ddg(vector data, int n)
+{
+    double **ret = calloc(n * n, sizeof(double));
+    return ret;
+}
+// Computes similarity between two vectors
+double sym_func(cord x, cord y)
+{
+    return 0;
+}
+
+// Computes similarity matrix
+double **sym(vector data, int n)
+{
     double **ret = calloc(n * n, sizeof(double));
     return ret;
 }
 
-double **sym(double **data, int n)
+// Computes normalized similarity matrix
+double **norm(vector data, int n)
 {
-    // Computes similarity matrix
     double **ret = calloc(n * n, sizeof(double));
     return ret;
 }
 
-double **norm(double **data, int n)
+// Matrix A: k*n, Matrix B: n*m, Matrix A*B: k*m
+double **mult_mat(double **matA, double **matB, int n, int k, int m)
 {
-    // Computes normalized similarity matrix
-    double **ret = calloc(n * n, sizeof(double));
-    return ret;
-}
-
-double **mult_mat(double **matA, double **matK, int n, int k, int m)
-{
-    // Matrix A: k*n, Matrix B: n*m, Matrix A*B: k*m
     double **ret = calloc(k * m, sizeof(double));
+    int i, j, p, sum;
+    for (i = 0; i < k; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            sum = 0;
+            for (p = 0; p < n; p++)
+            {
+                sum += matA[i][p] * matB[p][j];
+            }
+            ret[i][j] = sum;
+        }
+    }
     return ret;
 }
 
@@ -37,9 +67,10 @@ double **symnmf(double **H_i, double **W, int n, int k)
     return H_i;
 }
 
+// Prints a matrix of size n*m
 void print_mat(double **mat, int n, int m)
 {
-    // Prints a matrix of size n*m
+
     return;
 }
 
